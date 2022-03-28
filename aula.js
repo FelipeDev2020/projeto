deaea510a10922f547f3195884f39aa4ba8d030b
FelipeDev2020 <luisfelipexv20@gmail.com>
2022-03-28 20:47:55 -0300

    Atualizado

--- aula.js
+++ aula.js
@@ -1,9 +1,65 @@
-// let idade = 10
-// console.log(idade)
-// let altura = 180
-// console.log(altura)
+// let pessoa = { 
+//     nome: 'Luis',
+//     idade: 23,
+//     estaAprovado: true,
+//     sobrenome: 'Felipe',
+// };
 
-let nome = "Luis Felipe"
-let idade = 23
+// console.log(pessoa)
 
-console.log(`Olá meu nome é ${nome}, eu tenho ${idade} anos`)
+
+//arrays
+
+// let familia = [26, 45, 50, 18];
+
+// console.log(familia[1])
+// let i;
+// let nomeColega = ["Patrick", 21, "São Paulo"];
+// for(i = 0; i < nomeColega.length; i++){
+//     console.log(nomeColega[i]);
+// }
+
+// let corSite = "Azul";
+
+// function resetaCor(cor, tonalidade) {
+//     corSite = cor + tonalidade;
+// };
+
+// console.log(corSite);
+// resetaCor("verde", " claro");
+// console.log(corSite);
+
+// function dizerNome(){
+//     console.log('Luis');
+// };
+
+// dizerNome();
+
+// function MultiplicarPorDois(valor){
+//     return valor * 2;
+// };
+
+// console.log(MultiplicarPorDois(5));
+
+
+
+//operação ternaria 
+// let pontos = 200;
+// let tipo = pontos > 100 ? 'premiun' : 'comum';
+// console.log(tipo);
+
+// let a = 'vermelho';
+// let b = 'azul';
+// let c;
+
+// console.log(a);
+// console.log(b);
+// c = a;
+// a = b;
+// b = c;
+
+// console.log(a);
+
+// console.log(b);
+
+// Parei em 1 hora : 08 minutos 
