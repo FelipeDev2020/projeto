b1ed61e3e0a22d66c6a77505d9673575840dd373
FelipeDev2020 <luisfelipexv20@gmail.com>
2022-03-28 19:31:03 -0300

    atualização

---
+++ aula.js
@@ -0,0 +1,9 @@
+// let idade = 10
+// console.log(idade)
+// let altura = 180
+// console.log(altura)
+
+let nome = "Luis Felipe"
+let idade = 23
+
+console.log(`Olá meu nome é ${nome}, eu tenho ${idade} anos`)
